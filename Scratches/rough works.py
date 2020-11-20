e5e38633c459c6f4a39c9afc29e1407da7a5817f
Venkatesh <54614167+venkatesh547@users.noreply.github.com>
2020-11-20 14:23:17 +0530

    Create rough works.py

---
+++ Scratches/rough works.py
@@ -0,0 +1,9 @@
+# to calculate area of Rectnagle
+# hint area = width * height
+
+width = int(input('Enter the width of Rectangle: '))
+height = int(input('Enter the height of Rectnagle: '))
+
+area = width * height
+
+print(f'area is {area} sqft')
